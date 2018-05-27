4e4ef8e890ea7d62f45fc0a7c52afff4bb1023a3
davidlatuno <dslatuno@gmail.com>
2018-05-26 21:23:02 -0700

    Changed ico and added initial navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar = () =>
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">David Latuno</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Design</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">About</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+export default Navbar;
