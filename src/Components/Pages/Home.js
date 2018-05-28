6bb4c64a5c12fb8d2ae7fa4ec7bf7897a249d31f
davidlatuno <dslatuno@gmail.com>
2018-05-27 22:59:37 -0700

    Test picture with overlay

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import Navbar from "../Navbar";
 import Jumbotron from "../Jumbotron";
+import psychic from "./images/Psychic-game.png";
 import "./Pages.css";
 
-class App extends Component {
+class Home extends Component {
     render() {
         return (
             <div>
-            <Navbar />
-            <Jumbotron phrase="Front to Back" />
+                <Navbar />
+                <Jumbotron phrase="Front to Back" />
+                <div className="row">
+                    <div className="col-md-4">
+                        <div class="picContainer">
+                            <img src={psychic} alt="psychic-game" class="image" />
+                            <div class="overlay">
+                                <div class="text">Hello World</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                    </div>
+                    <div className="col-md-4">
+                    </div>
+                </div>
             </div>
-    )
+        )
     }
 }
 
-export default App;
+export default Home;
