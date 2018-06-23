70eca14c795a7c5447fb0fb5cadd9d8db0f822c1
davidlatuno <dslatuno@gmail.com>
2018-06-22 18:18:16 -0700

    Added resume and changed some css

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -17,6 +17,7 @@ class App extends Component {
                         <Card source={require("./images/gmail.png")} text="dslatuno@gmail.com" />
                         <Card source={require("./images/github.png")} text={<a href="https://github.com/davidlatuno" target="_blank"><button className="btn btn-dark">GitHub</button></a>} />
                         <Card source={require("./images/linkedin.png")} text={<a href="https://www.linkedin.com/in/david-latuno" target="_blank"><button className="btn btn-dark">LinkedIn</button></a>} />
+                        <Card source={require("./images/resume.png")} text={<a href="https://www.linkedin.com/in/david-latuno" target="_blank"><button className="btn btn-dark">Resume</button></a>} />
                         <Card source={require("./images/phone.png")} text="619-493-6024" />
                     </div>
                 </div>
