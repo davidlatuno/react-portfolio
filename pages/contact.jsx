4111f6a839a7459c091469916046dbd00e8507ef
davidlatuno <dslatuno@gmail.com>
2019-05-20 17:52:08 -0700

    Updated Resume Link

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -14,7 +14,7 @@ const App = () => (
         <Card source="../static/pics/gmail.png" text="dslatuno@gmail.com" />
         <Card source="../static/pics/github.png" text={<a href="https://github.com/davidlatuno" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">GitHub</button></a>} />
         <Card source="../static/pics/linkedin.png" text={<a href="https://www.linkedin.com/in/david-latuno" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">LinkedIn</button></a>} />
-        <Card source="../static/pics/resume.png" text={<a href="https://github.com/davidlatuno/react-portfolio/blob/master/static/pics/David%20Latuno%20Resume.pdf" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">Resume</button></a>} />
+        <Card source="../static/pics/resume.png" text={<a href="https://github.com/davidlatuno/react-portfolio/raw/master/static/pics/David%20Latuno%20Resume.pdf" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">Resume</button></a>} />
         <Card source="../static/pics/phone.png" text="619-493-6024" />
       </div>
     </div>
